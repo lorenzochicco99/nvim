bba08d7c21f3823394836ce2d899245e3a5e77a1
lorenzochicco <lorenzo.chicco@studenti.unitn.it>
2024-10-21 09:17:44 +0200

    Finalized Configuration

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,6 @@
+-- General configuration and basic remapping
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Note that this is not really necessary as we are using nvim-tree as a file explorer
 vim.o.number = true          -- Show absolute line numbers
 vim.o.relativenumber = true  -- Show relative line numbers
 vim.opt.tabstop = 4      -- Number of visual spaces per tab
